3951b5cf3570a182812e43cd522c2b3f4c801f50
omobankole <oluwabankyusuf1@gmail.com>
2025-03-31 22:59:05 +0100

    nlpc

--- src/constant/index.js
+++ src/constant/index.js
@@ -3,57 +3,44 @@ import Grazac from "../assets/images/Projects/grazac.jpg";
 import Grazac2 from "../assets/images/Projects/grazac-web.jpg";
 import Vast from "../assets/images/Projects/vast.png";
 import Atc from "../assets/images/Projects/atc.png";
-import Spen from "../assets/images/Projects/spen.png";
+import NLPC from "../assets/images/Projects/nlpc.jpeg";
 import Phast from "../assets/images/Projects/phast.png";
 
 export const data = [
   {
-    heading: "Spen - Managing business payments has never been easier",
-    img: Spen,
-    skills: "NextJs, Angular, Tailwind",
-    link1: "https://www.myspen.com/",
-    link2: "https://www.myspen.com/",
+    heading: "NLPC PFA - Start saving for your future",
+    img: NLPC,
+    skills: "React, Redux Toolkit, Redux Query, MUI",
+    link1: "https://www.nlpcpfa.com/",
+    link2: "https://www.nlpcpfa.com/",
   },
   {
     heading: "Passpoint - Beautiful Journeys Start Here",
     img: Passpoint,
-    skills: "NextJs, Tailwind",
-    link1: "https://mypasspoint.com/",
-    link2: "https://mypasspoint.com/",
+    skills: "NextJs, Redux Toolkit, Tailwind",
+    link1: "https://www.mypasspoint.com/",
+    link2: "https://www.mypasspoint.com/",
   },
   {
     heading: "Grazac - IDEAS, PEOPLE AND A SMART FUTURE",
     img: Grazac2,
     skills: "ReactJs • Scss",
-    link1: "https://grazac.com.ng/",
-    link2: "https://grazac.com.ng/",
+    link1: "https://www.grazac.com.ng/",
+    link2: "https://www.grazac.com.ng/",
   },
   {
     heading: "Grazac Academy - Upskilling You For The Future Of Work",
     img: Grazac,
     skills: "ReactJs • Scss",
-    link1: "https://grazacacademy.com/",
-    link2: "https://grazacacademy.com/",
+    link1: "https://www.grazacacademy.com/",
+    link2: "https://www.grazacacademy.com/",
   },
   {
     heading: "ATC Steam - ATC STEAM FOR CODING",
     img: Atc,
     skills: "NextJs",
-    link1: "https://steam.atc.com.ng/",
-    link2: "https://steam.atc.com.ng/",
+    link1: "https://steam.atchub.com.ng/",
+    link2: "https://steam.atchub.com.ng/",
   },
-  {
-    heading: "Phast - Giving you seamless and endless payment possibilities",
-    img: Phast,
-    skills: "React",
-    link1: "https://phastpay.com/",
-    link2: "https://phastpay.com/",
-  },
-  {
-    heading: "Vast - Worked on Admin Dashboard",
-    img: Vast,
-    skills: "NextJs • ReactJs",
-    link1: "https://usevast.com/",
-    link2: "https://usevast.com/",
-  },
+
 ];
